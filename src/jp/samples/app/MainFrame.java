7dc56f1f30db2bed455df32148c4dd11beadb95b
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-02-15 14:43:53 +0100

    Tab control sample (how to use)

--- src/jp/samples/app/MainFrame.java
+++ src/jp/samples/app/MainFrame.java
@@ -1,5 +1,8 @@
 package jp.samples.app;
 
+import jp.controls.tabControl.TabControl;
+import jp.controls.tabControl.TabItem;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
@@ -15,6 +18,25 @@ public class MainFrame extends JFrame {
 
     public MainFrame() {
         initializeComponents();
+        tabControlSample();
+    }
+
+    // how use tab-control
+    private void tabControlSample() {
+        JPanel tamItemPanel1 = makeContent(1);
+        JPanel tamItemPanel2 = makeContent(2);
+        JPanel tamItemPanel3 = makeContent(3);
+        JPanel tamItemPanel4 = makeContent(4);
+        JPanel tamItemPanel5 = makeContent(5);
+
+        TabControl tabControl = new TabControl();
+        tabControl.addTabItem(new TabItem("Tab item 1", tamItemPanel1));
+        tabControl.addTabItem(new TabItem("Tab item 2", tamItemPanel2));
+        tabControl.addTabItem(new TabItem("Tab item 3", tamItemPanel3));
+        tabControl.addTabItem(new TabItem("Tab item 4", tamItemPanel4));
+        tabControl.addTabItem(new TabItem("Tab item 5", tamItemPanel5));
+
+        contentPane.add(tabControl, BorderLayout.CENTER);
     }
 
     private JPanel makeContent(int index) {
